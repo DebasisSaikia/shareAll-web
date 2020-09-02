8dc0e125282a9232ea5f9aa28468c00bc6865742
Debasis Saikia <debasishsaikia136@gmail.com>
2020-09-02 12:25:15 +0530

    'changed_server'

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 const connectDB= require('./config/db');
  connectDB();
-
+app.use(express.json());
  app.set('views',path.join(__dirname,'/views'))
  app.set('view engine','ejs');
 
