3bb3753222889b17ba5527fa31a809cc471da357
Debasis Saikia <debasishsaikia136@gmail.com>
2020-09-02 12:59:26 +0530

    added_cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const app=express();
 const path=require('path');
+const cors=require('cors');
 
 
 const PORT=process.env.PORT || 3000
@@ -8,9 +9,15 @@ const PORT=process.env.PORT || 3000
 app.use(express.static('public'));
 app.use(express.json());
 
+//cors setup
+const corsOptions={
+    origin:process.env.ALLOWED_CLIENTS.split(',')
+}
+app.use(cors(corsOptions))
 const connectDB= require('./config/db');
  connectDB();
-app.use(express.json());
+
+
  app.set('views',path.join(__dirname,'/views'))
  app.set('view engine','ejs');
 
